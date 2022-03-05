031f8c2cc16f8eba21d7b1e6d02a2b085a8023a2
zxk <zxk.com>
2022-03-05 23:27:19 +0800

    zxk first commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
     }
     public static void sayHello() {
         System.out.println("hi,git");
-        System.out.println("11111111");
+        System.out.println("zxk11111111");
     }
-
 }
