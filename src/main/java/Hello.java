46be3a88079622b575226bc09290112e43f4f4cf
zxk <zxk.com>
2022-03-06 00:21:44 +0800

    zxk no pull,third push

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     }
     public static void sayHello() {
         System.out.println("hi,git");
-        System.out.println("zxk11111111");
+        System.out.println("zxk333");
         System.out.println("lds");
     }
 
